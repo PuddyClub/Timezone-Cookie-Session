c35881c4836a7c0e95f9efafcb476c4603b6a37a
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-02-05 13:48:49 -0300

    clockCfg added

--- files/constructor.js
+++ files/constructor.js
@@ -43,6 +43,24 @@ module.exports = function (req, data) {
     // Get List
     this.cfg.list = require('./selectList/timezone')(req.session.primary_timezone);
 
+    // Prepare Clock
+    if (typeof req.session.clock24 !== "string" || !req.session.clock24 || (req.session.clock24 !== "on" && req.session.clock24 !== "off")) {
+        req.session.clock24 = 'off';
+    }
+
+    this.clockCfg = {
+        typeOption: setConfig.clock24(req.session.clock24),
+        type24hours: req.session.clock24
+    };
+
+    if (this.clockCfg.type24hours === "off") {
+        this.clockCfg.format = "hh:mm:ss a";
+        this.clockCfg.format2 = "hh:mm a";
+    } else {
+        this.clockCfg.format = "HH:mm:ss";
+        this.clockCfg.format2 = "HH:mm";
+    }
+
     // Complete
     return this;
 

af9a134b1ddad80a012380a22c5e7bd32238af6f
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-02-05 18:54:53 -0300

    first base client made

--- files/clientWeb/base.js
+++ files/clientWeb/base.js
@@ -1,5 +1,37 @@
-module.exports = function (data) {
+module.exports = function (data = {}) {
 
-    return;
+    // Timezone
+    const tinyclock = {
+        extra_loops: [],
+        timezone: `{{primaryTimezone}}`,
+        newTimezone: moment
+            .tz
+            .guess()
+    };
+
+    // Compare Clock
+    if (`{{primaryTimezoneisAuto}}` === "true") {
+        if (tinyclock.timezone !== tinyclock.newTimezone && csrfToken) {
+            fetch("/setCookie", {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ type: 'timezone', value: tinyclock.newTimezone, csrfToken: csrfToken })
+            });
+        }
+    }
+
+    // Rest Set
+    tinyclock.secondary_timezone = `{{global.timezones.secondary_actived}}`;
+    tinyclock.formatDate = `dddd, MMMM Do YYYY`;
+    tinyclock.formatTime = `{{global.timezones.clock.format}}`;
+    tinyclock.is24hours = null;
+    tinyclock.mainTimezone = `{{global.timezones.mainTimezone}}`;
+    if (`{{type24hoursOn}}` === "true") { tinyclock.is24hours = true; } else { tinyclock.is24hours = false; }
+
+    // Complete
+    return tinyclock;
 
 };
--- files/clientWeb/index.js
+++ files/clientWeb/index.js
@@ -1,3 +1,6 @@
 module.exports = function (data) {
-    return require('./base').toString();
+    return require('./base').toString()
+    .replace(/\{\{primaryTimezone\}\}/g, this.cfg.actived)
+    .replace(/\{\{primaryTimezoneisAuto\}\}/g, this.cfg.auto.toString())
+    .replace(/\{\{type24hoursOn\}\}/g, (this.clockCfg.type24hours === "on").toString());
 };

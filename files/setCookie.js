7539e2b13af0241e9e135978f78f912778bb43e8
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-02-05 14:08:00 -0300

    preparing set cookie

---
+++ files/setCookie.js
@@ -0,0 +1,73 @@
+module.exports = async function (req, res, csrfToken = {
+    server: '',
+    now: ''
+}) {
+
+    // Yes
+    if (typeof req.body.value === "string") {
+
+        // Check Code
+        if (csrfToken.now !== csrfToken.server) {
+            res.status(401); res.json({ code: 401, text: 'CSRFToken!' });
+            return;
+        }
+
+        // Set Timezone
+        if (req.body.type === "timezone") {
+
+            req.body.value = req.body.value.substring(0, 100);
+
+            if (this.module.tz.zone(req.body.value)) {
+                req.session.timezone = req.body.value;
+            }
+
+        }
+
+        // Set Manual Timezone
+        else if (req.body.type === "manual_timezone") {
+
+            req.body.value = req.body.value.substring(0, 100);
+
+            if (this.module.tz.zone(req.body.value)) {
+                req.session.manual_timezone = req.body.value;
+            } else {
+                req.session.manual_timezone = 'auto';
+            }
+
+        }
+
+        // Set Secondary Timezone
+        else if (req.body.type === "secondary_timezone") {
+
+            req.body.value = req.body.value.substring(0, 100);
+
+            if (this.module.tz.zone(req.body.value)) {
+                req.session.secondary_timezone = req.body.value;
+            } else {
+                req.session.secondary_timezone = 'auto';
+            }
+
+        }
+
+        // 24 Clock
+        else if (req.body.type === "clock24") {
+            if (req.body.value === "on" || req.body.value === "off") {
+                req.session.clock24 = req.body.value;
+            }
+        }
+
+        // Result
+        return res.json({ success: true });
+
+    }
+
+    // Nope
+    else {
+
+        // Result
+        res.status(400);
+        return res.json({ success: false, error: 'Invalid request!', code: 400 });
+
+    }
+
+};

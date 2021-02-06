db1ee5686993e7d090ca05b11ae46ff6397cf4d7
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-02-06 15:27:44 -0300

    base data cache added

--- api/clientWeb/index.js
+++ api/clientWeb/index.js
@@ -1,12 +1,21 @@
+// Base Data
+let baseData = null;
+
 module.exports = function (data = {}) {
-    return require('./base').toString()
-    .replace('{{setCookieURL}}', this.urls.setCookie)
-    .replace('{{utcValue}}', this.utcValue)
-    .replace('{{primaryTimezone}}', this.cfg.actived)
-    .replace('{{secondaryTimezone}}', this.cfgSecondary.actived)
-    .replace('{{clockFormat}}', this.clockCfg.format)
-    .replace('{{clockFormat2}}', this.clockCfg.format2)
-    .replace('{{mainTimezone}}', this.main)
-    .replace('{{primaryTimezoneisAuto}}', this.cfg.auto.toString())
-    .replace('{{type24hoursOn}}', (this.clockCfg.type24hours === "on").toString());
+
+    // Cache Value
+    if (typeof baseData !== "string") { baseData = require('./base').toString(); }
+
+    // Return Data
+    return baseData
+        .replace('{{setCookieURL}}', this.urls.setCookie)
+        .replace('{{utcValue}}', this.utcValue)
+        .replace('{{primaryTimezone}}', this.cfg.actived)
+        .replace('{{secondaryTimezone}}', this.cfgSecondary.actived)
+        .replace('{{clockFormat}}', this.clockCfg.format)
+        .replace('{{clockFormat2}}', this.clockCfg.format2)
+        .replace('{{mainTimezone}}', this.main)
+        .replace('{{primaryTimezoneisAuto}}', this.cfg.auto.toString())
+        .replace('{{type24hoursOn}}', (this.clockCfg.type24hours === "on").toString());
+
 };
